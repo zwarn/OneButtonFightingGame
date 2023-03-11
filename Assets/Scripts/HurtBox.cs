d4fd67968ea9c08616830835161b82b63f947307
Michael Budahn <Michael.Budahn@gmail.com>
2023-03-11 20:52:00 +0100

    spawnable hiteffect

--- Assets/Scripts/HurtBox.cs
+++ Assets/Scripts/HurtBox.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 public class HurtBox : MonoBehaviour
 {
     public PlayerController owner;
+    public GameObject hitEffect;
 
     private void OnTriggerEnter(Collider other)
     {
         owner.OnHurt();
         owner.Opponent.OnHit();
+
+        if (hitEffect != null)
+        {
+            var position = (owner.transform.position + owner.Opponent.transform.position) / 2;
+            Instantiate(hitEffect, position, Quaternion.identity);
+        }
     }
 }
